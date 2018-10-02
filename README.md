f958ba242ec0476114c8aef3a401e4e24fb4e8e7
Michael <thesentientgelatin@gmail.com>
2018-10-01 22:16:40 -0700

    error warnings
    *Added a friiiiiick ton of error warnings for index and value errors. This code is now more try/except statements than it isn't.
    
    *Updated readme
    *So it's basically done.

--- README.md
+++ README.md
@@ -3,5 +3,6 @@ Ok so a few months ago my mom needed to check the occurences of items in a sprea
 This program does that automatically. (This could've been a command line program. Would only take 5 minutes to cobble together, but I wanted it to be usable by Generation X! and also GUI experience)
 GUI made with Tkinter (The question: why didn't I use Java for this?? Making GUIs for that is so much easier!! The answer: stubbornness!)
 
-# it's not done yet.
-its functional, but it needs the GUI to be fixed. (Currently it's **VERY** wonky). It also doesn't have any try/handle for errors. So just don't do anything to throw an error!
+# so is it done?
+It's functional. The GUI is about as function as I want it to be. It's 99% error foolproof (unless you try to work without a spreadsheet I suppose), but I don't really care to put more work into it.
+I hate Tkinter now. My next step will probably be recoding it in Java or Jython for experience, learning, and beautification of the program.
--- spreadsheetitemcounter.py
+++ spreadsheetitemcounter.py
@@ -4,39 +4,67 @@ from tkinter.filedialog import *
 from collections import Counter
 
 spreadsheet = ""
+errorRaised = ""
 def importFile():
     global spreadsheet
     global tcv
     spreadsheet = askopenfilename(parent=root, filetypes =[("CSV files", ".csv")])
     print(spreadsheet)
-    subButton.grid(row=8, column=2, sticky="S")
+    subButton.grid(row=10, column=2, sticky="S")
 def openAdv():
-    global cir
-    global ctc
+    global conInRow   #contains in row
+    global colToCheck #columns to check for conInRow
     advButton.grid_remove()
-    Label(root, text="Only count in rows that contain:").grid(row=11,column=1)
-    Label(root, text="in column:").grid(row=12,column=1)
-    cir = Entry(root)
-    cir.grid(row=11,column=2)
-    ctc = Entry(root, width=3)
-    ctc.grid(row=12,column=2)
-    cir.insert(END,"")
-    ctc.insert(END,"")
-    Label(root, text="LEAVE BLANK if you want to count in every single row").grid(row=13,column=1,columnspan=2)
+    Label(root, text="Only count in rows that contain:").grid(row=6,column=1,columnspan=2)
+    Label(root, text="in column:").grid(row=7,column=2)
+    colToCheck = Entry(root, width=3,bg=None)
+    colToCheck.grid(row=7,column=3)
+    conInRow = Entry(root, width=5)
+    conInRow.grid(row=6,column=3)
+    colToCheck.insert(END,"")
+    conInRow.insert(END,"")
+    Label(root, text="LEAVE BLANK if you want to count in every single row").grid(row=8,column=1,columnspan=3)
 def process(): # processes the file!
     opts = []
+    global errorRaised
+    colToCheck.configure(bg="White")
+    cEntry.configure(bg="White")
+    errorRaised.set("")
     with open(spreadsheet) as csvfile:
         readCSV = csv.reader(csvfile, delimiter=",")
-        c = int(cEntry.get()) - 1 # which column to count from || INDEXES AT 0!!
-            
+        try:
+            c = int(cEntry.get()) - 1
+        except ValueError:
+            cEntry.configure(bg="Red")
+            errorRaised.set("You may only use numbers to pick columns")    
         for row in readCSV:
             if labeled.get() == 1 and readCSV.line_num == 1:
                 continue #sends you to top instead of continuing down
-            if ctc.get() == "" or cir.get() == "":
-                opts.append(row[c])
-            else:
-                if row[int(ctc.get())-1] == cir.get():
+            if colToCheck.get() == "" or conInRow.get() == "":
+                try:
                     opts.append(row[c])
+                except IndexError:
+                    cEntry.configure(bg="Red")
+                    errorRaised.set("That isn't a valid column")
+            else:
+                try:
+                    if row[int(colToCheck.get())-1] == conInRow.get():
+                        try:
+                            opts.append(row[c])
+                        except ValueError:
+                            cEntry.configure(bg="Red")
+                            errorRaised.set("You may only use numbers to pick columns")
+                        except IndexError:
+                            cEntry.configure(bg="Red")
+                            errorRaised.set("That isn't a valid column")
+                except ValueError:
+                    colToCheck.configure(bg="Red")
+                    errorRaised.set("You may only use numbers to pick columns")
+                    break
+                except IndexError:
+                    colToCheck.configure(bg="Red")
+                    errorRaised.set("That isn't a valid column")
+                    break
                 
         print("The counts are:")
         counts = Counter(opts)
@@ -47,33 +75,35 @@ def process(): # processes the file!
         tcv.set(y)
                
 root = Tk()
-root.geometry("700x700")
+root.geometry("330x700")
 root.resizable(0,0)
 textCounts = Label(root, text="")
 text1 = Label(root, text="Select your file", font="50")
 imButton = Button(root, text="Import", command=importFile, width=14, height=2)
 subButton = Button(root, text="Submit", command=process, width=14, height=2)
 text2 = Label(root, text="Are your columns labeled at the top row?", font=45)
-text3 = Label(root, text="What column do you want to count in?", font=45)
+text3 = Label(root, text="What column do you want to count in?", font=45, background=None)
 cEntry = Entry(root, width = 2)
 advButton = Button(root, text="Advanced", width=7, command=openAdv)
 tcv = StringVar()
 textCounts = Label(root, textvariable=tcv, font=45) #tcv - textCounts Variable
-textCounts.grid(row=9, column=2)
-cir = Entry()
-ctc = Entry()
+textCounts.grid(row=11, column=1, columnspan=3)
+conInRow = Entry()
+colToCheck = Entry()
+errorRaised = StringVar()
+errorWarning = Label(root, textvariable=errorRaised)
 
 text1.grid(row=0, column=2, pady=10)
-imButton.grid(row=1, padx=0, column = 2)
-text2.grid(row=2, column=1, columnspan=3)
+imButton.grid(row=1, column=2)
+text2.grid(row=2, column=0, columnspan=4)
 labeled = IntVar()
 Radiobutton(root, text="Yes", variable=labeled, value=1).grid(row=3, column=1)
 Radiobutton(root, text="No", variable=labeled, value=0).grid(row=3, column=3)
-text3.grid(row=6, column=1, columnspan=2)
-cEntry.grid(row=6, column=3)
+text3.grid(row=4, column=0, columnspan=3)
+cEntry.grid(row=4, column=3)
 cEntry.insert(END, 1)
-advButton.grid(row=11,column=1)
+advButton.grid(row=6,column=2)
+errorWarning.grid(row=12, column = 2)
 
-root.grid_rowconfigure(2, minsize=50)
-root.grid_rowconfigure(6, minsize=50)
-root.grid_columnconfigure(0, minsize=50)
+for i in range(10):
+    root.grid_rowconfigure(i, minsize=30)
